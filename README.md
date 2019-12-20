720b4e03db27613b1ead5da60cf5c39ea38195af
Christoph Bachhuber <cbachhuber89@gmail.com>
2019-12-20 22:24:56 +0100

    Introduce setup arguments, encapsulate setup steps into functions

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# My Ubuntu Post Installation Script and Config Files
+# Ubuntu Post Installation Script and Config Files
 
-Tested for Ubuntu 18.04 LTS.
+Tested for Ubuntu 18.04 LTS. The script and config files in this folder allow you to quickly set up your Ubuntu installation with a reasonable developer configuration.
 
-I suggest cloning this repository to ~/.dotfiles. Though any other location will work as well. Just make sure to not touch the folder after you started relying on the dotfiles in it.
+I suggest cloning this repository to ~/.dotfiles, though any other location will work as well. Just make sure to not move the folder after you started relying on the dotfiles in it.
 
 ## Downloading
 
@@ -19,3 +19,12 @@ Execute script `setup.sh` in the toplevel folder of this repository
 ```
 
 You will be asked to enter your sudo password for the apt operations.
+
+### Setup arguments
+
+By default, `setup.sh` will work through all setup steps such a OS tweaks, program installation, git configuration etc. If you only want select a subset of these steps, use the below flags. As soon as a flag is given, the other steps are not implicitly executed, they need to be called explicitly per flag as well.
+
+
+- Flag `-o` or `--os-tweaks` walks you through OS tweaking steps such as key repeat adjustment and configuration of gnome shell extensions
+- Flag `-p` or `--programs` installs programs such as chromium, vlc, gimp, and dev tools such as python3, terminator, git, and zsh.
+- Flag `-g` or `--configure-git` guides you through common git configuration steps such as setting up your git user name and mail, git pager, global excludesFile, and git aliases.
--- setup.sh
+++ setup.sh
@@ -2,6 +2,46 @@
 
 set -eu
 
+# TODO split me up into sub functions, called from separate scripts
+
+OS_TWEAKS=false
+PROGRAMS=false
+CONFIGURE_GIT=false
+
+# Let the user clone this repo to any location
+DOTFILES_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+CONFIG_FOLDER=""$DOTFILES_PATH"/config"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -o|--os-tweaks)
+        OS_TWEAKS=true
+        shift # past argument
+        ;;
+        -p|--programs)
+        PROGRAMS=true
+        shift # past argument
+        ;;
+        -g|--configure-git)
+        CONFIGURE_GIT=true
+        shift # past argument
+        ;;
+        *)    # unknown option
+        POSITIONAL+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [ "$OS_TWEAKS" = false ] && [ "$PROGRAMS" = false ] && [ "$CONFIGURE_GIT" = false ]; then
+    OS_TWEAKS=true
+    PROGRAMS=true
+    CONFIGURE_GIT=true
+fi
+
 prompt_message_and_wait_for_input()
 {
     echo "$1"
@@ -19,75 +59,87 @@ run_command_and_ask_to_close()
     sleep 0.5
 }
 
-# Let the user clone this repo to any location
-DOTFILES_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-CONFIG_FOLDER=""$DOTFILES_PATH"/config"
 
-# OS tweaks
-GNOME_TWEAKS_INSTALLED=$(dpkg -l | grep gnome-tweaks | wc -l)  # gnome-tweaks is not part of the default Ubuntu packages: http://releases.ubuntu.com/bionic/ubuntu-18.04.3-desktop-amd64.manifest
-if [ $GNOME_TWEAKS_INSTALLED = 0 ]; then
-    sudo apt install -y gnome-tweaks gnome-shell-extension-weather gnome-shell-extension-system-monitor gnome-shell-extension-impatience
-    printf "Gnome extensions require a logout and login to become visible in gnome settings.
-    Please log out and back in, and restart this script."
-    return 0
-else
-    echo "Welcome back! Let's continue with gnome extension settings."
-    echo " 1. To use the nice dark mode in your OS, change Appearance->Themes->Applications to 'Adwaita-dark'."
-    echo " 2. Also consider changing your cursor appearance"
-    echo " 3. Under Top Bar, enable date and week numbers"
-    echo " 4. Under 'Extensions', enable and configure openweather and System-monitor."
-    run_command_and_ask_to_close gnome-tweaks
-fi
+walk_through_os_tweaks()
+{
+    echo "Walking through OS tweaks"
+    GNOME_TWEAKS_INSTALLED=$(dpkg -l | grep gnome-tweaks | wc -l)  # gnome-tweaks is not part of the default Ubuntu packages: http://releases.ubuntu.com/bionic/ubuntu-18.04.3-desktop-amd64.manifest
+    if [ $GNOME_TWEAKS_INSTALLED = 0 ]; then
+        sudo apt install -y gnome-tweaks gnome-shell-extension-weather gnome-shell-extension-system-monitor gnome-shell-extension-impatience
+        printf "Gnome extensions require a logout and login to become visible in gnome settings.
+        Please log out and back in, and restart this script."
+        return 0
+    else
+        echo "Welcome back! Let's continue with gnome extension settings."
+        echo " 1. To use the nice dark mode in your OS, change Appearance->Themes->Applications to 'Adwaita-dark'."
+        echo " 2. Also consider changing your cursor appearance"
+        echo " 3. Under Top Bar, enable date and week numbers"
+        echo " 4. Under 'Extensions', enable and configure openweather and System-monitor."
+        run_command_and_ask_to_close gnome-tweaks
+    fi
 
-echo "Add a german, english, or other keyboard layout, if you like."
-run_command_and_ask_to_close gnome-control-center region
-echo "Fix your key repeat delay and rate by clicking 'Typing->Repeat Keys'. Be careful to not set the delay too low, and the rate too high. Experiment with the text editor on the side. Note: the speed slider is inverse! Pushing it leftwards means higher repeat rate."
-run_command_and_ask_to_close gedit & gnome-control-center universal-access
-echo "Fix your automatic suspend delays: click on 'Automatic suspend', then choose to your liking."
-run_command_and_ask_to_close gnome-control-center power
+    echo "Add a german, english, or other keyboard layout, if you like."
+    run_command_and_ask_to_close gnome-control-center region
+    echo "Fix your key repeat delay and rate by clicking 'Typing->Repeat Keys'. Be careful to not set the delay too low, and the rate too high. Experiment with the text editor on the side. Note: the speed slider is inverse! Pushing it leftwards means higher repeat rate."
+    run_command_and_ask_to_close gedit & gnome-control-center universal-access
+    echo "Fix your automatic suspend delays: click on 'Automatic suspend', then choose to your liking."
+    run_command_and_ask_to_close gnome-control-center power
+}
 
-# TODO Browser selection
-echo "Which browser should we install? (chromium/chrome/no_additional)?"
-sudo apt install -y chromium-browser
+install_programs()
+{
+    echo "Installing recommended programs"
+    # TODO Browser selection
+    echo "Which browser should we install? (chromium/chrome/no_additional)?"
+    sudo apt install -y chromium-browser
 
-# Media playback and recording
-sudo apt install -y vlc kazam ubuntu-restricted-extras
+    # Media playback and recording
+    sudo apt install -y vlc kazam ubuntu-restricted-extras
 
-# Image and graphics tools
-echo "Install gimp and inkscape?"
-sudo apt install -y gimp inkscape pdftk imagemagick
-# TODO fix imagemagick permissions
+    # Image and graphics tools
+    echo "Install gimp and inkscape?"
+    sudo apt install -y gimp inkscape imagemagick
+    # TODO fix imagemagick permissions
 
-# Other tools
-sudo apt install -y unrar htop iotop
+    # Other tools
+    sudo apt install -y unrar htop iotop
 
-# Essential dev tools
-sudo apt install -y neovim zsh git terminator curl python3-dev python3-pip python3-setuptools build-essential cmake libgtest-dev
-sudo apt install -y powerline fonts-powerline
-prompt_message_and_wait_for_input "Let terminator show more lines: right-click into this text, click 'Preferences->Profile->Scrolling'. Under 'Scrollback', set the number of lines to something more reasonable, e.g. 5000 lines."
+    # Essential dev tools
+    sudo apt install -y neovim zsh git terminator curl python3-dev python3-pip python3-setuptools build-essential cmake libgtest-dev tree
+    sudo apt install -y powerline fonts-powerline
+    prompt_message_and_wait_for_input "Let terminator show more lines: open terminator, right-click into the empty space, click 'Preferences->Profile->Scrolling'. Under 'Scrollback', set the number of lines to something more reasonable, e.g. 5000 lines."
+}
 
-# Git configuration
-read -p "Enter your git user name: " GIT_NAME
-read -p "Enter your git mail address: " GIT_MAIL
-git config --global user.name $GIT_NAME
-git config --global user.email $GIT_MAIL
-git config --global core.pager 'less -F -X'  # use less only if you output does not fit to the screen
-git config --global core.excludesFile "$CONFIG_FOLDER"/global_gitignore
-echo "[include]
-	path = /home/$(whoami)/.dotfiles/config/gitconfig" >> ~/.gitconfig  # $HOME expansion not supported in gitconfig, need absolute path
+configure_git()
+{
+    echo "Configuring git"
+    # Git configuration
+    read -p "Enter your git user name: " GIT_NAME
+    read -p "Enter your git mail address: " GIT_MAIL
+    git config --global user.name $GIT_NAME
+    git config --global user.email $GIT_MAIL
+    git config --global core.pager 'less -F -X'  # use less only if you output does not fit to the screen
+    git config --global core.excludesFile "$CONFIG_FOLDER"/global_gitignore
+    echo "[include]
+    	path = /home/$(whoami)/.dotfiles/config/gitconfig" >> ~/.gitconfig  # $HOME expansion not supported in gitconfig, need absolute path
 
-# Git Editor
-while true; do
-read -p "Would you like to use vim as git editor? (y/n) " yn
-case $yn in
-    [Yy]* ) git config --global core.editor 'vim'; break;;  # more handy than nano when closing with 'ZZ' (discard with ':cq')
-    [Nn]* ) echo "Not installing"; break;;
-    * ) echo "Please answer yes or no.";;
-esac
-done
+    # Git Editor
+    while true; do
+    read -p "Would you like to use vim as git editor? (y/n) " yn
+    case $yn in
+        [Yy]* ) git config --global core.editor 'vim'; break;;  # more handy than nano when closing with 'ZZ' (discard with ':cq')
+        [Nn]* ) echo "Not installing"; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+    done
+}
+
+if [ "$OS_TWEAKS" = true ]; then walk_through_os_tweaks; fi
+if [ "$PROGRAMS" = true ]; then install_programs; fi
+if [ "$CONFIGURE_GIT" = true ]; then configure_git; fi
 
 # TODO zsh setup (default shell) and configuration
-# TODO Check if ~/.zshrc already exists, back up if yes. For all existing config files, use local backup folder
+# TODO Check if ~/.zshrc already exists, back up if yes. For all existing config files, use local backup folder: .dotfiles/my_stuff/backups/zshrc
 #ln -s "$CONFIG_FOLDER"/zshrc ~/.zshrc
 
 # TODO neovim configuration. Use vimrc!
