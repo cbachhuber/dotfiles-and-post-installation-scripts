32ad9ff12e666c8dc69204a88451180f4d817d37
Christoph Bachhuber <christoph.bachhuber@nokia.com>
2020-10-19 10:29:41 +0200

    Fix paths

--- tm1_clang_and_gcc.sh
+++ tm1_clang_and_gcc.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -eu
 
+SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
-local_pipeline -c
+"$SCRIPT_DIR"/tm1_local_pipeline.sh -c
 
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
-local_pipeline -c
+"$SCRIPT_DIR"/tm1_local_pipeline.sh -c
 
