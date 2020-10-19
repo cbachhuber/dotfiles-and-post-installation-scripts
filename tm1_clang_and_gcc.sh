ed498eba69aadeedb685ef366c933f84f66e0839
Christoph Bachhuber <christoph.bachhuber@nokia.com>
2020-10-19 10:46:01 +0200

    Add messages

--- tm1_clang_and_gcc.sh
+++ tm1_clang_and_gcc.sh
@@ -5,9 +5,11 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
+printf "Build using gcc\n"
 "$SCRIPT_DIR"/tm1_local_pipeline.sh -c
 
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
+printf "\n\nBuild using clang\n\n"
 "$SCRIPT_DIR"/tm1_local_pipeline.sh -c
 
