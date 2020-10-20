6767b7fb43342cda9802e1b93fcb8e65db8fae81
Christoph Bachhuber <christoph.bachhuber@nokia.com>
2020-10-20 20:52:01 +0200

    gcc build only, full pass with clang

--- tm1_clang_and_gcc.sh
+++ tm1_clang_and_gcc.sh
@@ -6,10 +6,10 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 printf "Build using gcc\n"
-"$SCRIPT_DIR"/tm1_local_pipeline.sh -c
+"$SCRIPT_DIR"/tm1_local_pipeline.sh -c -b -hm
 
 export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 printf "\n\nBuild using clang\n\n"
-"$SCRIPT_DIR"/tm1_local_pipeline.sh -c
+"$SCRIPT_DIR"/tm1_local_pipeline.sh -c -hm
 
